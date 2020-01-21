92d39e1f4573ed97a050b7caf6fa28f931b6d5cf
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-01-21 21:15:01 +0000

    Improved GenerateNoisyLine.py more randomness

--- RANSAC/GenerateNoisyLine.py
+++ RANSAC/GenerateNoisyLine.py
@@ -1,10 +1,16 @@
-import numpy as np
-
+#
+#Use this script to generate a mono chrome picture with 1 line and noise
+#This will generate a random straight line 
+#  *)left edge to right edge
+#  *)or top edge to bottom edge
+#  *)scatter the picture with salt-pepper noise
+#
 
 import numpy as np
 import os
 import skimage
 import PlotLineOnNumpyArray as plotter
+import random
 #
 #Create blank image
 #
@@ -21,10 +27,19 @@ image_noisy=skimage.util.random_noise(img,mode="s&p",seed=None, clip=True,salt_v
 #Generate a straight line
 #
 num_points=30
-start_x=img_width * 0.8
-start_y=img_height * 0.8
-end_x= img_width * 0.2
-end_y=img_height * 0.2
+if (random.randint(0,1) == 0):
+    #generate a line starting from a random point at the bottom edge and going up to a random point on the top edge
+    start_x=random.random() * (img_width-1)
+    start_y=0
+    end_x=random.random() * (img_width-1)
+    end_y=img_height-1
+else:
+    #generate a line starting from a random point on the left edge and going up to a random point on the right edge
+    start_x=0
+    start_y=random.random() * (img_height-1)
+    end_x= img_width-1
+    end_y=random.random() * (img_height-1)
+
 image_with_line=plotter.PlotLineOnArray (image_noisy,start_x,start_y,end_x,end_y,num_points)
 
 image_result=image_with_line
@@ -33,7 +48,10 @@ image_result=image_with_line
 #Save the image to disk
 #
 folder_script=os.path.dirname(__file__)
-filename="NoisyImage.%d.png" % img_back_color
+folder_results=os.path.join(folder_script,"./out/")
+count_of_files=len(os.listdir(folder_results))
+
+filename=("NoisyImage.%d.png" % count_of_files)
 file_result=os.path.join(folder_script,"./out/",filename)
 skimage.io.imsave(file_result,image_result)
 #
