2d904ee5f2f1ae5af9e747909d8336bebd9a26b9
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-02-29 15:37:06 +0000

    Completed CircleModel.GenerateModelFrom3Points

--- Common/CircleModel.py
+++ Common/CircleModel.py
@@ -11,8 +11,29 @@ class CircleModel(object):
     def __str__(self):
         display=("X=%f Y=%f R=%f" % (self.X,self.Y,self.R))
         return display
+
+#
+#Determine the center and radius of a circle which passes through the 3 points
+#    
+def GenerateModelFrom3Points(p1:pmodel.Point, p2:pmodel.Point, p3:pmodel.Point)->CircleModel:
+    x1=p1.X
+    x2=p2.X
+    x3=p3.X
+    y1=p1.Y
+    y2=p2.Y
+    y3=p3.Y
+    c = (x1-x2)**2 + (y1-y2)**2
+    a = (x2-x3)**2 + (y2-y3)**2
+    b = (x3-x1)**2 + (y3-y1)**2   
+    s = 2*(a*b + b*c + c*a) - (a*a + b*b + c*c)
+
+    px = (a*(b+c-a)*x1 + b*(c+a-b)*x2 + c*(a+b-c)*x3) / s
+    py = (a*(b+c-a)*y1 + b*(c+a-b)*y2 + c*(a+b-c)*y3) / s 
+    ar = a**0.5
+    br = b**0.5
+    cr = c**0.5 
+    r = ar*br*cr / ((ar+br+cr)*(-ar+br+cr)*(ar-br+cr)*(ar+br-cr))**0.5
+
+    circ=CircleModel(px,py,r)
     
-def GenerateModelFrom3Points(p1:pmodel.Point, p2:pmodel.Point, p3:pmodel.Point):
-    #TODO generate circle model
-    circ=CircleModel(0,0,1)
     return circ
--- Common/test_CircleModel.py
+++ Common/test_CircleModel.py
@@ -1,5 +1,6 @@
 import unittest
 import CircleModel as cmodel
+import Point as pmodel
 
 class Test_CircleModel(unittest.TestCase):
     def test_When_Constructed_All_Properties_Must_Be_Initialized(self):
@@ -14,5 +15,35 @@ class Test_CircleModel(unittest.TestCase):
         self.assertTrue(display.find("X=100.1") >= 0)
         self.assertTrue(display.find("Y=200.2") >= 0)
         self.assertTrue(display.find("R=300.3") >= 0)
+
+    #Points  (-1,0) (0,1)  (1,0)
+    def test_GenerateModelFrom3pmodels_PassingThrough_1_0_And_0_1_minus1_0(self):
+        p_1_0=pmodel.Point(0,1)
+        p_0_0=pmodel.Point(1,0)
+        p_minus1_0=pmodel.Point(-1,0)
+
+
+        c1=cmodel.GenerateModelFrom3Points(p_0_0,p_1_0,p_minus1_0)
+        #Assert on radius
+        self.assertAlmostEquals(c1.R, 1.0,1)
+        #Assert on center X,Y
+        self.assertAlmostEquals(c1.X, 0.0,1)
+        self.assertAlmostEquals(c1.Y, 0.0,1)
+
+
+    #Points  (-1,1) (0,2)  (1,1)
+    def test_GenerateModelFrom3pmodels_PassingThrough_1_1_And_0_2_minus1_1(self):
+        p_1_0=pmodel.Point(1,1)
+        p_0_0=pmodel.Point(0,2)
+        p_minus1_0=pmodel.Point(-1,1)
+
+
+        c1=cmodel.GenerateModelFrom3Points(p_0_0,p_1_0,p_minus1_0)
+        #Assert on radius
+        self.assertAlmostEquals(c1.R, 1.0,1)
+        #Assert on center X,Y
+        self.assertAlmostEquals(c1.X, 0.0,1)
+        self.assertAlmostEquals(c1.Y, 1.0,1)
+
 if __name__ == '__main__':
     unittest.main()
