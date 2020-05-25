11565ffb1d57e32ae7ad365a79b6070971ba5b7a
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-05-25 17:18:15 +0100

    Removed MultipleLines

--- Common/test_GradientDescentCircleFitting.py
+++ Common/test_GradientDescentCircleFitting.py
@@ -14,9 +14,27 @@ class Test_test_GradientDescentCircleFitting(unittest.TestCase):
         expected_lrate=1.1
         helper=gdescent.GradientDescentCircleFitting(None,expected_lrate,expected_list)
         self.assertEqual(expected_lrate,helper._learningrate)
-        self.assertassertTrue (p1 in helper._points)
-        self.assertassertTrue (p2 in helper._points)
+        self.assertTrue (p1 in helper._points)
+        self.assertTrue(p2 in helper._points)
 
+
+    def test_3points_around_origin_unit_radius(self):
+        p1=pmodel.Point(+1,0)
+        p2=pmodel.Point(+0,1)
+        p3=pmodel.Point(-1,0)
+
+        expected_list=list()
+        expected_list.append(p1)
+        expected_list.append(p2)
+        expected_list.append(p3)
+
+        expected_lrate=0.1
+        helper=gdescent.GradientDescentCircleFitting(None,expected_lrate,expected_list)
+        result:cmodel.CircleModel =helper.FindBestFittingCircle()
+        self.assertAlmostEquals(result.R, 1.0);
+        self.assertAlmostEquals(result.X, 0.0);
+        self.assertAlmostEquals(result.Y, 0.0);
+                        
 
     def test_Skeleton(self):
         self.fail("Not implemented")
