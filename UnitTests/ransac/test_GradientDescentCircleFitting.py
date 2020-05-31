c6a1be3b177f2f62556f63f1119400f270056aac
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-05-31 01:18:45 +0100

    Implemented default parametes for the class GradientDescentCircleFitting

--- UnitTests/ransac/test_GradientDescentCircleFitting.py
+++ UnitTests/ransac/test_GradientDescentCircleFitting.py
@@ -5,7 +5,7 @@ from RANSAC.Common import CircleModel
 from RANSAC.Algorithm import GradientDescentCircleFitting
 
 class Test_test_GradientDescentCircleFitting(unittest.TestCase):
-    def test_Constructor(self):
+    def test_Constructor_With_Learning_Rate(self):
         p1=Point(1,1)
         p2=Point(2,2)
         expected_list=list()
@@ -13,12 +13,25 @@ class Test_test_GradientDescentCircleFitting(unittest.TestCase):
         expected_list.append(p2)
 
         expected_lrate=1.1
-        helper=GradientDescentCircleFitting(None,expected_lrate,expected_list)
+        helper=GradientDescentCircleFitting(None,points=expected_list,learningrate=expected_lrate)
         self.assertEqual(expected_lrate,helper._learningrate)
         self.assertEqual(len(expected_list),len(helper._points))
         self.assertTrue (p1 in helper._points)
         self.assertTrue(p2 in helper._points)
 
+    def test_Constructor_With_Default_Learning_Rate(self):
+        p1=Point(1,1)
+        p2=Point(2,2)
+        expected_list=list()
+        expected_list.append(p1)
+        expected_list.append(p2)
+
+        expected_lrate=0.05
+        helper=GradientDescentCircleFitting(None,points=expected_list)
+        self.assertEqual(expected_lrate,helper._learningrate)
+        self.assertEqual(len(expected_list),len(helper._points))
+        self.assertTrue (p1 in helper._points)
+        self.assertTrue(p2 in helper._points)
 
     #
     #A very basic test with the following 3 points only. 
@@ -45,6 +58,8 @@ class Test_test_GradientDescentCircleFitting(unittest.TestCase):
         self.assertAlmostEquals(result.X, 0.0, delta=delta);
         self.assertAlmostEquals(result.Y, 0.0, delta=delta);
         
+    def test_5points_around_origin_unit_radius(self):
+        self.fail("Not implemented")
 
     def test_When_GD_Is_Invoked_With_less_than_3_points_Exception_MustBe_Thrown(self):
         self.fail("Not implemented")
