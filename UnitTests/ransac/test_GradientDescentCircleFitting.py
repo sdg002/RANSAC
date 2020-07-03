e1401cd0bb133b08c40b8c3b8e18ff19c459c4eb
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-07-03 23:28:54 +0100

    added new GradientDescent test case for timing improvement

--- UnitTests/ransac/test_GradientDescentCircleFitting.py
+++ UnitTests/ransac/test_GradientDescentCircleFitting.py
@@ -121,7 +121,7 @@ class Test_test_GradientDescentCircleFitting(unittest.TestCase):
             self.assertEquals(message,"Need 3 or more points");
             pass
 
-    def test_large_circle_50X50_no_noise(self):
+    def test_large_circle_50X50_no_noise_1(self):
         folder_script=os.path.dirname(__file__)
         filename_input="NoisyCircle_x_-10_y_-14.png"
         file_noisy_line=os.path.join(folder_script,"./data/",filename_input)
@@ -145,6 +145,32 @@ class Test_test_GradientDescentCircleFitting(unittest.TestCase):
         self.assertAlmostEquals(result.R, 48.0, delta=delta);
         self.assertAlmostEquals(result.X, -10.0, delta=delta);
         self.assertAlmostEquals(result.Y, -14.0, delta=delta);
+        pass
+
+    def test_large_circle_50X50_no_noise_2(self):
+        folder_script=os.path.dirname(__file__)
+        filename_input="NoisyCircle_x_6_y_-30_r_118.162.png"
+        file_noisy_line=os.path.join(folder_script,"./data/",filename_input)
+        np_image=skimage.io.imread(file_noisy_line,as_gray=True)
+        lst_points=Util.create_points_from_numpyimage(np_image)
+
+        helper=GradientDescentCircleFitting(None,lst_points,learningrate=0.4,iterations=5000)
+        result:CircleModel =helper.FindBestFittingCircle()
+        #
+        #Superimpose the new line over the image
+        #
+        folder_results=os.path.join(folder_script,"../out/")
+        count_of_files=len(os.listdir(folder_results))
+        filename_results=("%s.%d.png" % (__name__,count_of_files) )
+        file_result=os.path.join(folder_results,filename_results)
+        new_points=CircleModel.generate_points_from_circle(result)
+        np_superimposed=Util.superimpose_points_on_image(np_image,new_points,100,255,100)
+        skimage.io.imsave(file_result,np_superimposed)
+
+        delta=10
+        self.assertAlmostEquals(result.R, +118.0, delta=delta);
+        self.assertAlmostEquals(result.X, +06.0, delta=delta);
+        self.assertAlmostEquals(result.Y, -30.0, delta=delta);
         pass
 
 if __name__ == '__main__':
